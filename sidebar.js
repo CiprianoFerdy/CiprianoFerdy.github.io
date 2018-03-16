c54d7e302243f980d82085f74ff80da4bd86f9e8
Ferdy Cipriano <ferdycip@gmail.com>
2018-03-15 20:12:22 -0600

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,12 @@
+
+function toggleNav() {
+       var updateElement = document.getElementById("menu-icon");
+    //toggle adds a class if it's not there or removes it if it is.
+    updateElement.classList.toggle("open");
+  
+       var updateElement2 = document.getElementById("menu-content");
+    //toggle adds a class if it's not there or removes it if it is.
+    updateElement2.classList.toggle("open");
+
+      
+}              
