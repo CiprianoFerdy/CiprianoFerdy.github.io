cc18cfe82504f2bc29b2779da4709efda3d21ce4
Ferdy Cipriano <ferdycip@gmail.com>
2018-04-08 00:31:24 -0600

    Add files via upload

---
+++ CarpenterWebsite/js/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var uluru = {lat: 14.877123, lng: -91.537596};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
